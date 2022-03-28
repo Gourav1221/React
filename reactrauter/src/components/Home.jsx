5f46c8d9fb689c3bb56dcd1299924dafa9c32b7f
Suraj kumar <90993575+Surajmicky@users.noreply.github.com>
2022-03-28 21:32:26 +0530

    d

--- reactrauter/src/components/Home.jsx
+++ reactrauter/src/components/Home.jsx
@@ -2,20 +2,10 @@ import axios from "axios";
 import { useState,useEffect } from "react";
 
 export const Home = () => {
- const [data,setdata] = useState([]);
- useEffect(() =>{
-   
-  getdata()
-    
- },[])
-
- async function getdata(){
-     let data =  await axios.get("https://github.com/typicode/json-server");
-     console.log(data)
- }
+ 
     return (
         <div>
-           
+           home
         </div>
     )
 }
--- reactrauter/src/components/Product.jsx
+++ reactrauter/src/components/Product.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
 export const Product = () => {
+    const [data,setdata] = useState([]);
+ useEffect(() =>{
+   
+  getdata()
+    
+ },[])
+
+ async function getdata(){
+     let data =  await axios.get("https://github.com/typicode/json-server");
+     console.log(data)
+ }
   return (
     <div>Product</div>
   )
