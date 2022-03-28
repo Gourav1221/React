292e66935244bad5548301c5008f6946c7360bca
Gourav1221 <gouravmisra111@gmail.com>
2022-03-28 21:03:48 +0530

    push login signup

--- reactrauter/src/components/Navbar/Navbar.jsx
+++ reactrauter/src/components/Navbar/Navbar.jsx
@@ -15,6 +15,7 @@ export const Navbar = () => {
         {/* Home component will have `/` route and classname as `home`  */}
 
         <button className="home"><Link to="/" element={<Home />}>Home</Link></button> 
+        <button className="home"><Link to="/" element={<Home />}>Login / sign up</Link></button> 
         {/* <button className="history"><Link to="/section/history" element={<Section/>}>History</Link></button> */}
         {/* <button className="Mystery"><Link to="/section/Mystery" element={<Section/>}>Mystery</Link></button> */}
         {/* <button className="Mythology"><Link to="/section/Mythology" element={<Section/>}>Mythology</Link></button> */}
